d46e53c4ceec56a4394865b622bbdddbdb19ae40
achumuhi <120632726+achumuhi@users.noreply.github.com>
2023-01-27 10:17:13 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>home</title>
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style1.css">
+  <!-- Favicon -->
+  <link href="img/favicon.ico" rel="icon">
+
+<!-- Google Web Fonts -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Barlow:wght@500;600;700&family=Open+Sans:wght@400;600&display=swap" rel="stylesheet"> 
+
+<!-- Icon Font Stylesheet -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+<!-- Libraries Stylesheet -->
+<link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+<!-- Customized Bootstrap Stylesheet -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Template Stylesheet -->
+<link href="css/style.css" rel="stylesheet">
+</head>
+<body>
+   
+<!-- header section starts  -->
+
+<section class="header">
+
+   <a href="home.php" class="logo">ZETAMATALS.</a>
+
+   <nav class="navbar">
+      <a href="#home">home</a>
+      <a href="#about">about</a>
+      <a href="#package">services</a>
+      <a href="#contact">contact</a>
+   </nav>
+
+   <div id="menu-btn" class="fas fa-bars"></div>
+
+</section>
+
+<!-- header section ends -->
+
+<!-- home section starts  -->
+
+<section class="home" id="home">
+
+   <div class="swiper home-slider">
+
+      <div class="swiper-wrapper">
+
+         <div class="swiper-slide slide" style="background:url(images/8.jpg) no-repeat">
+            <div class="content">
+               <!--<span>explore, discover, travel</span>-->
+               <h3 style="font-size:50px">We provide better conditions for your better trading</h3>
+               <!--<a href="package.php" class="btn">discover more</a>-->
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/6.jfif) no-repeat">
+            <div class="content">
+            <h3 style="font-size:50px">options trading made easy</h3>
+               <!--span>explore, discover, travel</span>
+               <h3>discover the new places</h3>
+               <a href="package.php" class="btn">discover more</a>-->
+            </div>
+         </div>
+
+         <div class="swiper-slide slide" style="background:url(images/cd.jfif) no-repeat">
+            <div class="content">
+            <h3 style="font-size:50px">think local sell global</h3>
+               <!--<span>explore, discover, travel</span>
+               <h3>make your tour worthwhile</h3>
+               <a href="package.php" class="btn">discover more</a>-->
+            </div>
+         </div>
+         
+      </div>
+
+      <div class="swiper-button-next"></div>
+      <div class="swiper-button-prev"></div>
+
+   </div>
+
+</section>
+
+<!-- home section ends -->
+
+<!-- services section starts  -->
+
+<section class="services" id="services">
+
+   <h1 class="heading-title">commodity </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/9.jfif" alt="">
+        
+      </div>
+
+      <div class="box">
+         <img src="images/aa.jpg" alt="">
+        
+      </div>
+
+      <div class="box">
+         <img src="images/d.jpg" alt="">
+         
+      </div>
+
+      <div class="box">
+         <img src="images/e.jfif" alt="">
+        
+      </div>
+
+      <div class="box">
+         <img src="images/1a.jfif" alt="">
+        
+      </div>
+
+      <div class="box">
+         <img src="images/g.jpg" alt="">
+        
+      </div>
+
+   </div>
+
+</section>
+
+<!-- services section ends -->
+
+<!-- home about section starts  -->
+
+<section class="home-about" id="about">
+
+   <div class="image">
+      <img src="images/t.jpeg" alt="">
+   </div>
+
+   <div class="content">
+      <h3>about us</h3>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita et, recusandae nobis fugit modi quibusdam ea assumenda, nulla quisquam repellat rem aliquid sequi maxime sapiente autem ipsum? Nobis, provident voluptate?</p>
+      <!--<a href="about.php" class="btn">read more</a>-->
+   </div>
+
+</section>
+
+<!-- home about section ends -->
+
+<!-- home packages section starts  -->
+
+<section class="home-packages" id="package">
+
+   <h1 class="heading-title"> SERVICES </h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <div class="image">
+            <img src="images/trade1.jpg" alt="">
+         </div>
+         <div class="content">
+            <h3>TRADING</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, sint!</p>
+            <!--<a href="book.php" class="btn">book now</a>-->
+         </div>
+      </div>
+
+      <div class="box">
+         <div class="image">
+            <img src="images/trade2.jfif" alt="">
+         </div>
+         <div class="content">
+            <h3>COMMODITY</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, sint!</p>
+            <!--<a href="book.php" class="btn">book now</a>-->
+         </div>
+      </div>
+      
+      <div class="box">
+         <div class="image">
+            <img src="images/trade3.jfif" alt="">
+         </div>
+         <div class="content">
+            <h3>eXPORTS AND IMPORTS</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos, sint!</p>
+            <!--<a href="book.php" class="btn">book now</a>-->
+         </div>
+      </div>
+
+   </div>
+
+   <!--<div class="load-more"> <btn class="btn"> load more</div>-->
+
+</section>
+
+<!-- home packages section ends -->
+
+<!-- home offer section starts  -->
+
+<!--<section class="home-offer" id="home-offer">-->
+<section id="contact">
+   <div class="content">
+     
+    <!--<div class="container-fluid bg-secondary px-0">-->
+      
+        <div class="row g-0">
+            <div class="col-lg-6 py-6 px-5">
+                <h1 class="display-5 mb-4">Contact </h1>
+                <form>
+                    <div class="row g-3">
+                        <div class="col-6">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="form-floating-1" placeholder="John Doe">
+                                <label for="form-floating-1">Full Name</label>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="form-floating">
+                                <input type="email" class="form-control" id="form-floating-2" placeholder="name@example.com">
+                                <label for="form-floating-2">Email address</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-floating">
+                                <input type="text" class="form-control" id="form-floating-3" placeholder="Subject">
+                                <label for="form-floating-3">Subject</label>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-floating">
+                                <textarea class="form-control" placeholder="Message" id="form-floating-4" style="height: 150px"></textarea>
+                                <label for="form-floating-4">Message</label>
+                              </div>
+                        </div>
+                        <div class="col-12">
+                            <button class="btn btn-primary w-100 py-3" type="submit">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-6" style="min-height: 400px;">
+                <div class="position-relative h-100">
+                    <!--<iframe class="position-relative w-100 h-100"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d126137.22009862089!2d77.27388432036231!3d8.899280057076009!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b042956c432db6d%3A0x8820676f06f6f6c1!2sMSP%20Technology!5e0!3m2!1sen!2sin!4v1670663295463!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"
+                        frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>-->
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- home offer section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="#home"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="#about"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="#package"> <i class="fas fa-angle-right"></i> services</a>
+         <!--<a href="book.php"> <i class="fas fa-angle-right"></i> book</a>-->
+      </div>
+
+      <div class="box">
+         <!--<h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> about us</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>-->
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> contact@zetamatals.in  </a>
+         <a href="#"> <i class="fas fa-map"></i> chennai, india - 400104 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="#"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="#"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="#"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="#"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </div>
+
+   <!--<div class="credit"> created by <span>mr. web designer</span> | all rights reserved! </div>
+
+</section>
+
+<!-- footer section ends -->
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
